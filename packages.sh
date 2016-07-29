8f0e24b8a3cb538a1a522ba0e6768c0618fe6898
Alex Howard <alex@howardsupply.com>
2016-07-28 22:18:22 -0700

    correct `ubuntu-make` package name

--- packages.sh
+++ packages.sh
@@ -4,4 +4,4 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 
 echo "Installing packages..."
-sudo apt-get update && sudo apt-get install nodejs git wget curl umake sublime-text-installer -y
+sudo apt-get update && sudo apt-get install nodejs git wget curl ubuntu-make sublime-text-installer -y
