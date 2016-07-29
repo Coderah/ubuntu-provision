63fcfd5964ecd08ee3a271c85ed36099e20726d7
Alex Howard <alex@howardsupply.com>
2016-07-28 22:16:06 -0700

    correct -y switch for add-apt-repository calls

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 echo "Adding apt repositories..."
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 
 echo "Installing packages..."
 sudo apt-get update && sudo apt-get install nodejs git wget curl umake sublime-text-installer -y
