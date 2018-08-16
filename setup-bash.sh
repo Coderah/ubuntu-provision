bebc86bcf5a59fa098f0b60cdd01773826a071bf
Coderah <alex@howardsupply.com>
2018-08-16 16:50:27 -0700

    add git tab completion for `g` alias

--- setup-bash.sh
+++ setup-bash.sh
@@ -1,2 +1,4 @@
 echo "Installing sexy-bash-prompt..."
 (cd /tmp && git clone --depth 1 https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
+
+echo "\nsource /usr/share/bash-completion/completions/git\n__git_complete g __git_main" >> ~/.bashrc
