92f3cef35318a2f596ea8d80c99031282abaa560
Coderah <alex@howardsupply.com>
2019-09-27 10:08:22 -0700

    update

--- configs.sh
+++ configs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 yellow='\033[1;33m'
 
-sublimePath="~/config/sublime-text-3"
-sublimeUserPath="${sublimePath}/Packages/User"
+#sublimePath="~/config/sublime-text-3"
+#sublimeUserPath="${sublimePath}/Packages/User"
 
 echo "Configuring packages..."
 
@@ -14,7 +14,7 @@ echo "${yellow}Don't forget to set git config email manually.";
 cp ./configs/vs-code.json ~/.config/Code/User -f
 
 # sublime
-cp ./configs/sublime.json ${sublimeUserPath}/Preferences.sublime-settings
-cp ./configs/sublime-packages.json ${sublimeUserPath}/Package Control.sublime-settings
+#cp ./configs/sublime.json ${sublimeUserPath}/Preferences.sublime-settings
+#cp ./configs/sublime-packages.json ${sublimeUserPath}/Package Control.sublime-settings
 
-wget https://sublime.wbond.net/Package%20Control.sublime-package ${sublimePath}/Installed\ Packages/
+#wget https://sublime.wbond.net/Package%20Control.sublime-package ${sublimePath}/Installed\ Packages/
